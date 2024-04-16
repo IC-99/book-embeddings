0f8589843c056b10d33bb79ede372179e8180ab3
IC-99 <92325157+IC-99@users.noreply.github.com>
2024-04-16 17:47:06 +0200

    added methods in PQtree.py

--- python code/PQtree.py
+++ python code/PQtree.py
@@ -48,8 +48,80 @@ def perm(root):
         res.extend(new_permutations)
     return res
 
+def is_value_in_children(root, x):
+    if not root:
+        return False
+    for child in root.children:
+        if child.value == x:
+            return True
+    return False
+
+def remove_child_by_value(root, x):
+    if not root:
+        return False
+    for i in range(len(root.children)):
+        if root.children[i].value == x:
+            root.children.pop(i)
+            return True
+    return False
+
 def isolate(root, x):
-    pass
+    if not root:
+        return None
+    if not root.children:
+        if root.value == x:
+            return root
+    if is_value_in_children(root, x):
+        if x.value == 'Q':
+            if root.children[0].value == x or root.children[-1].value == x:
+                return root
+            else:
+                return None
+        elif x.value == 'P':
+            remove_child_by_value(root, x)
+            return PQtreeNode('Q', [PQtreeNode(x), root])
+    else:
+        t_i = None
+        index_of_t_i = 0
+        for i in range(len(root.children)):
+            if x in get_yield(root.children[i]):
+                t_i = root.children[i]
+                index_of_t_i = i
+                break
+        t_ii = isolate(t_i, x)
+        if not t_ii:
+            return None
+        else:
+            root.children.pop(index_of_t_i)
+            root.children.insert(index_of_t_i, t_ii)
+
+            if root.value == 'P':
+                root.children.pop(index_of_t_i)
+                t_ii.children.append(root)
+                return t_ii
+            elif root.value == 'Q':
+                if root.children[0] == t_ii:
+                    root.children.pop(0)
+                    for i in range(len(t_ii.children) - 1, -1, -1):
+                        root.children.insert(0, t_ii.children[i])
+                    return root
+                elif root.children[-1] == t_ii:
+                    root.children.pop(len(root.children) - 1)
+                    for child in t_ii.children:
+                        root.children.append(child)
+                    return root
+                else:
+                    return None
+                
+def print_PQtree(root, depth = 1):
+    if not root:
+        print('---')
+    if not root.children:
+        print(' ' * depth, root.value)
+    for child in root.children:
+        print('-' * depth)
+        print_PQtree(child, depth + 1)
+
 
 tree1 = PQtreeNode('Q', [PQtreeNode(1),
                          PQtreeNode(3),
@@ -73,3 +145,5 @@ tree3 = PQtreeNode('P', [PQtreeNode('A'),
 
 print(perm(tree2))
 print(perm(tree1))
+
+print_PQtree(tree1)
