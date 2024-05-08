1257dda8d94aa5e64ceadf7929ac3b2fe2f7762a
Ivan Carlini <ivancarlini99@gmail.com>
2024-05-08 20:32:46 +0200

    Update test.c

--- webassembly/test.c
+++ webassembly/test.c
@@ -4,8 +4,8 @@
 
 int main() {
     printf("Hello, WebAssembly!\n");
-    srand(time(NULL));   // Initialization, should only be called once.
-    int r = rand();      // Returns a pseudo-random integer between 0 and RAND_MAX.
+    srand(time(NULL));
+    int r = rand();
     printf("numero casuale selezionato: %d\n", r);
     return 0;
 }
