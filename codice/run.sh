7c6110fefca295ca84130076799ebf5560a08496
Ivan Carlini <ivancarlini99@gmail.com>
2024-07-03 20:37:07 +0200

    Update run.sh

--- codice/run.sh
+++ codice/run.sh
@@ -1,5 +1,18 @@
-#! /bin/bash
+#!/bin/bash
 
-g++ -I/home/ivan/OGDF/include -I/home/ivan/OGDF/src -o file.o -c $1
-g++ -o binary -L/home/ivan/OGDF file.o -lOGDF -lCOIN -lgcov
-./binary
+# Controlla che un argomento sia passato allo script
+if [ -z "$1" ]; then
+    echo "Uso: $0 <file.cpp>"
+    exit 1
+fi
+
+# Compila il file sorgente in un file oggetto
+g++ -I/home/ivan/OGDF/include -I/home/ivan/OGDF/src -o file.o -c "$1" || { echo "Compilazione fallita"; exit 1; }
+
+# Link del file oggetto in un eseguibile
+g++ -o binary -L/home/ivan/OGDF file.o -lOGDF -lCOIN -lgcov || { echo "Link fallito"; exit 1; }
+
+# Esegue il binario
+./binary || { echo "Esecuzione fallita"; exit 1; }
+
+echo "Script eseguito con successo"
